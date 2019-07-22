72c730cab767460f4761eecea1cae22de3cb7dec
Christer Eriksson <christer@dogtail.se>
2019-07-22 17:38:00 +0200

    Change filesystemwatcher and add nginx

--- run.ps1
+++ run.ps1
@@ -1,1 +1,3 @@
-docker run -it --rm -v C:\Users\chris\Desktop\feed:/app --entrypoint=bash  mdev/documentation:v0.1 
+#docker run -it --rm -v C:\Users\chris\Desktop\feed:/feed --entrypoint=bash  mdev/documentation:v0.1 
+docker run --name mdev-doc-privatgallerycreator -v C:\Users\chris\Desktop\feed:/feed -d --rm mdev/documentation:v0.1 
+docker run --name mdev-doc-nginx -v C:\Users\chris\Desktop\feed:/usr/share/nginx/html:ro -d --rm -p 8080:80 nginx 
---
+++ stop.ps1
@@ -0,0 +1,3 @@
+#docker run -it --rm -v C:\Users\chris\Desktop\feed:/feed --entrypoint=bash  mdev/documentation:v0.1 
+docker stop mdev-doc-privatgallerycreator  
+docker stop mdev-doc-nginx 
